ac26f019e6cddc376dfb38b85865cf871692ca2c
Liam <liam@myoxygen.co.uk>
2022-01-03 01:24:54 +0000

    Added clickthrough components and content

--- src/sections/Clickthrough/index.tsx
+++ src/sections/Clickthrough/index.tsx
@@ -1,5 +1,26 @@
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import Button from "../../components/Button";
+
 const Clickthrough = () => {
-  return <div>Clickthrough</div>;
+  const title = "OpenType Feature and Variable Fonts";
+  const btnText = "Try For Free";
+  return (
+    <Box>
+      <Card>
+        <CardContent>
+          <Box sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography variant="h2">{title}</Typography>
+              <Button text={btnText} />
+            </Box>
+            <Box>
+              <img src="#" alt="clickthrough" />
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 };
 
 export default Clickthrough;
